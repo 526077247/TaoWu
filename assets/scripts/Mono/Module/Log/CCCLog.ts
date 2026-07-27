261bc2ee7d58baa378a4d55e775f26a66c9616c4
Tailor <526077247@qq.com>
2026-07-27 10:43:49 +0800

    添加一键install bat，修复log bug

--- assets/scripts/Mono/Module/Log/CCCLog.ts
+++ assets/scripts/Mono/Module/Log/CCCLog.ts
@@ -5,27 +5,27 @@ import { debug, error, log, warn } from "cc";
 export class CCCLog implements ILog {
     trace(message: string): void;
     trace(message: string, ...args: any[]): void {
-        log(string.format(message, args));
+        log(string.format(message, ...args));
     }
 
     warning(message: string): void;
     warning(message: string, ...args: any[]): void {
-        warn(string.format(message, args));
+        warn(string.format(message, ...args));
     }
 
     info(message: string): void;
     info(message: string, ...args: any[]): void {
-        log(string.format(message, args));
+        log(string.format(message, ...args));
     }
 
     debug(message: string): void;
     debug(message: string, ...args: any[]): void {
-        debug(string.format(message, args));
+        debug(string.format(message, ...args));
     }
 
     error(message: string): void;
     error(message: string, ...args: any[]): void {
-        error(string.format(message, args));
+        error(string.format(message, ...args));
     }
     
 }
--- assets/scripts/Mono/Module/Log/ConsoleLog.ts
+++ assets/scripts/Mono/Module/Log/ConsoleLog.ts
@@ -4,27 +4,27 @@ import * as string from "../../Helper/StringHelper"
 export class ConsoleLog implements ILog {
     trace(message: string): void;
     trace(message: string, ...args: any[]): void {
-        console.log(string.format(message, args));
+        console.log(string.format(message, ...args));
     }
 
     warning(message: string): void;
     warning(message: string, ...args: any[]): void {
-        console.warn(string.format(message, args));
+        console.warn(string.format(message, ...args));
     }
 
     info(message: string): void;
     info(message: string, ...args: any[]): void {
-        console.log(string.format(message, args));
+        console.log(string.format(message, ...args));
     }
 
     debug(message: string): void;
     debug(message: string, ...args: any[]): void {
-        console.debug(string.format(message, args));
+        console.debug(string.format(message, ...args));
     }
 
     error(message: string): void;
     error(message: string, ...args: any[]): void {
-        console.error(string.format(message, args));
+        console.error(string.format(message, ...args));
     }
     
 }
